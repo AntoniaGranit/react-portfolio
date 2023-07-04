79ad1b47fb1a001fa6c8071c5b669e44fc4e3974
AntoniaGranit <antonia.granit@gmail.com>
2023-07-04 14:26:38 +0300

    add timed close menu function to navlink click

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-boolean-value */
-import React from 'react';
+import React, { useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-scroll';
 import { Envelope, Kanban, Braces, Gear, ArrowUpSquare } from 'react-bootstrap-icons';
@@ -7,44 +7,61 @@ import { MenuLink, MenuIcon } from '../Styles/Fonts';
 import './Menu.css';
 
 const MenuComponent = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleStateChange = (state) => {
+    setMenuOpen(state.isOpen);
+  };
+
+  const closeMenu = () => {
+    setTimeout(() => setMenuOpen(false), 1000)
+  };
+
   return (
     <div>
       <Menu
         right
-        width={260}>
+        width={260}
+        isOpen={menuOpen}
+        onStateChange={handleStateChange}>
         <Link
           activeClass="active"
           to="tech"
           smooth
-          duration={1000}>
+          duration={1000}
+          onClick={closeMenu}>
           <MenuLink><MenuIcon><Braces /></MenuIcon> Tech stack</MenuLink>
         </Link>
         <Link
           activeClass="active"
           to="projects"
           smooth
-          duration={1000}>
+          duration={1000}
+          onClick={closeMenu}>
           <MenuLink><MenuIcon><Kanban /></MenuIcon> Projects</MenuLink>
         </Link>
         <Link
           activeClass="active"
           to="skills"
           smooth
-          duration={1000}>
+          duration={1000}
+          onClick={closeMenu}>
           <MenuLink><MenuIcon><Gear /></MenuIcon> Skills</MenuLink>
         </Link>
         <Link
           activeClass="active"
           to="contactme"
           smooth
-          duration={1000}>
+          duration={1000}
+          onClick={closeMenu}>
           <MenuLink><MenuIcon><Envelope /></MenuIcon> Contact me</MenuLink>
         </Link>
         <Link
           activeClass="active"
           to="landing"
           smooth
-          duration={1000}>
+          duration={1000}
+          onClick={closeMenu}>
           <MenuLink><MenuIcon><ArrowUpSquare /></MenuIcon> To the top</MenuLink>
         </Link>
       </Menu>
